908055998326d40364e272be4c92a78825f8d3f9
Cainã <moaraadrean@gmail.com>
2020-11-04 22:32:10 -0300

    Adicionando funções para manipular usuários no JSON

--- frontend/src/components/user/UserCrud.jsx
+++ frontend/src/components/user/UserCrud.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 import Main from '../template/Main'
 
 const headerProps ={
@@ -7,7 +8,41 @@ const headerProps ={
     subtitle: 'Cadastro de usuários: Incluir, Listar, Alterar e Excluir'
 }
 
+// Restaurar estado inicial
+
+const baseUrl = 'http://localhost:3001/users'
+const initialState = {
+    user: {name: '', email: ''},
+    list: []
+}
+
+
 export default class UserCrud extends Component {
+
+    state = {...initialState} // Estadp iniciar
+
+    clear() {
+        this.setState({ user: initialState.user })
+    }
+
+    save () {
+        const user = this.state.user
+        const method = user.id ? 'put' : 'post' // Se usuário tiver id será alterado, se não, será adicionado
+        const url = user.id ? `${baseUrl}/${user.id}` : baseUrl // Caso usuário esteja setado, acrescente o ID na url
+        axios[method](url, user)
+            .then(resp => {
+                const list = this.getUptadedList(resp.data) // Atualiza a lista
+                this.setState({user: initialState.user, list}) // Retorna lista para estado inicial
+            })
+    }
+
+    getUptadedList(user) {
+        const list = this.state.list.filter(u => u.id !== user.id) // Passa lista sem o usuário adicionado e depois o adiciona na primeira posição
+        list.unshift(user)
+        return list
+    }
+    
+
     render() {
         return (
             <Main {...headerProps}>
