9874878d129a3ee3e31486ed02223bc935e9f80f
Cainã <moaraadrean@gmail.com>
2020-11-05 20:22:16 -0300

    Criando funções para persistir dados no backend

--- frontend/src/components/user/UserCrud.jsx
+++ frontend/src/components/user/UserCrud.jsx
@@ -25,6 +25,7 @@ export default class UserCrud extends Component {
         this.setState({ user: initialState.user })
     }
 
+    // Função para Salvar
     save () {
         const user = this.state.user
         const method = user.id ? 'put' : 'post' // Se usuário tiver id será alterado, se não, será adicionado
@@ -35,18 +36,70 @@ export default class UserCrud extends Component {
                 this.setState({user: initialState.user, list}) // Retorna lista para estado inicial
             })
     }
-
+    
+    // Função para incluir
     getUptadedList(user) {
         const list = this.state.list.filter(u => u.id !== user.id) // Passa lista sem o usuário adicionado e depois o adiciona na primeira posição
         list.unshift(user)
         return list
+    }   
+
+    uptadeField(event) {
+        const user = { ...this.state.user }
+        user[event.target.name] = event.target.value
+        this.setState({ user })
+    } 
+
+    renderForm() {
+        return (
+            <div className="form">
+                <div className="row">
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>Nome</label>
+                            <input type="text" className="form-control"
+                                name="name"
+                                value={this.state.user.name}
+                                onChange={e => this.uptadeField(e)}
+                                placeholder="Digite o nome..." />
+                        </div>
+                    </div>
+
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>E-mail</label>
+                            <input type="text" className="form-control"
+                            name="name"
+                            value={this.state.user.email}
+                            onChange={e => this.UptadeField(e)}
+                            placeholder="Digite o e-mail..."/>
+                        </div>
+                    </div>
+                </div>
+
+                <hr/>
+                <div className="row">
+                    <div className="col-12 d-flex justify-content-end">
+                        <button className="btn btn-primary"
+                            onClick={e => this.save(e)}>
+                            Salvar
+                        </button>
+
+                        <button className="btn btn-secondary ml-2"
+                            onClick={e => this.save(e)}>
+                            Cancelar
+                        </button>
+                    </div>
+                </div>
+            </div>
+        )
     }
     
 
     render() {
         return (
             <Main {...headerProps}>
-                Cadastro de Usuário
+                {this.renderForm()}
             </Main>
         )
     }
