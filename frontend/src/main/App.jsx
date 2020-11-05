e2e4585014fcbb764b2adf9814d8f2a32520e7ff
Cainã <moaraadrean@gmail.com>
2020-11-04 21:37:58 -0300

    Editando menu e criando arquivo de navegação

--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 import React from 'react'
 
 import Footer from '../components/template/Footer'
-import Main from '../components/template/Main'
+import Home from '../components/home/Home'
 import Logo from '../components/template/Logo'
 import Nav from'../components/template/Nav'
 
@@ -12,9 +12,8 @@ import Nav from'../components/template/Nav'
 export default props => 
     <div className="app">
         <Logo />
-        <Nav  icon="home"/>
-        <Main icon="home" title="Início"
-            subtitle="Segundo Projeto do capítulo de React."/>
+        <Nav  />
+        <Home />
         <Footer />
     </div>
 
